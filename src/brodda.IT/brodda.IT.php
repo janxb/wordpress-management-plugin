7846e418abd287e8c726aed9060c87ccaac532a9
Jan Brodda <github@janbrodda.de>
2025-02-12 08:38:49 +0000

    .

---
+++ src/brodda.IT/brodda.IT.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name: brodda.IT wordpress management
+ * Requires Plugins: aryo-activity-log, http-headers
+*/
+
+define('WP_POST_REVISIONS', 10);
+
+update_option('admin_email', 'wordpress@brodda.it');
+delete_option('new_admin_email');
+
+add_action('admin_init', function () {
+    remove_submenu_page('logdash_activity_log', 'logdash_settings');
+});
+
+add_filter("avf_debugging_info", function ($debug) {
+    return '';
+}, 10, 1);
